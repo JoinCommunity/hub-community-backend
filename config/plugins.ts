4c46fcc59e60732964d0f1fe54a75db2bcb9580e
Pedro Goiania <pedro.paulods@hotmail.com>
2025-09-17 23:03:06 -0300

    added configuration to send email

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+export default () => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: "smtp.mailgun.org", // Mailgun's SMTP server
+        port: 587,
+        auth: {
+          user: "eventando@8020digital.com.br",
+          pass: "F1r3wall@0212",
+        },
+        secure: false,
+      },
+      settings: {
+        defaultFrom: "contato@8020digital.com.br",
+        defaultReplyTo: "contato@8020digital.com.br",
+      },
+    },
+  },
+});
